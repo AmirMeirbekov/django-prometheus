286810fc62f629beb2aa6abd6fc201afb965ea6d
Amir Meirbekov <amir@easyanticheat.net>
2018-04-01 00:10:01 +0300

    Adds possibility to prefix metrics url path with secret token

--- django_prometheus/urls.py
+++ django_prometheus/urls.py
@@ -1,7 +1,13 @@
+import os
+
 from django.conf.urls import url
 from django_prometheus import exports
 
+url_prefix = ''
+if os.getenv('PROMETHEUS_URL_TOKEN'):
+    url_prefix = '{}/'.format(os.getenv('PROMETHEUS_URL_TOKEN'))
+
 urlpatterns = [
-    url(r'^metrics$', exports.ExportToDjangoView,
+    url(r'^{}metrics$'.format(url_prefix), exports.ExportToDjangoView,
         name='prometheus-django-metrics'),
 ]
